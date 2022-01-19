363959081580506354e07407a7bb0d0d622488d0
Didier Dardevet <didier.dardevet@gmail.com>
2022-01-20 00:57:46 +0100

    add models work on add contact template

--- contactapp/admin.py
+++ contactapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Contact, Group
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = [
+        "full_name",
+        "phone",
+        "email",
+        "group",
+        "date_created"
+    ]
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ["name"]
--- contactapp/models.py
+++ contactapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Contact(models.Model):
+    full_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    group = models.ForeignKey(to=Group, on_delete=models.SET_NULL, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
+
+
