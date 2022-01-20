c705be8140edb6046a390ba81a5f542f3dbc7b75
Didier Dardevet <didier.dardevet@gmail.com>
2022-01-20 18:55:05 +0100

    working on add contact / contact detail

--- contactapp/urls.py
+++ contactapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add-contact/', views.add_contact, name='add-contact'),
+    path('contact-detail/<int:pk>/', views.contact_detail, name='contact-detail')
 ]
--- contactapp/views.py
+++ contactapp/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact, Group
+from .forms import ContactForm
 
 
 def index(request):
-    return render(request, 'contactapp/index.html')
+    contacts = Contact.objects.all()
+    context = {
+        'contacts': contacts
+    }
+    return render(request, 'contactapp/index.html', context=context)
+
+
+def add_contact(request):
+    groups = Group.objects.all()
+
+    print(groups)
+    form = ContactForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+    else:
+        form = ContactForm()
+
+    context = {
+        'groups': groups,
+        'form': form}
+
+    return render(request, 'contactapp/addcontact.html', context=context)
+
+
+def contact_detail(request, pk):
+    contact = Contact.objects.get(pk=pk)
+    context = {'contact': contact}
+    return render(request, 'contactapp/detail.html', context=context)
